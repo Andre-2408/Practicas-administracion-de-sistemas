30ce091e79ca36fb6add9ef36c328e29041eca9f
Andre-2408 <ikerleyva2005@gmail.com>
2026-02-02 02:53:06 -0700

    arreglo disco Win

--- check_status.ps1
+++ check_status.ps1
@@ -1,15 +1,15 @@
 
-Write-Host "NOMBRE DEL EQUIPO:" -ForegroundColor Yellow
+Write-Host "Nombre del equipo"
 Write-Host $env:COMPUTERNAME
 Write-Host ""
 
-Write-Host "DIRECCIÓN IP:" -ForegroundColor Yellow
-$ipAddresses = Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress -notlike "127.*" }
+Write-Host "DIRECCION IP: "
+$ipAddresses = Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.IPAddress -notlike "127.*"}
 foreach ($ip in $ipAddresses) {
     Write-Host "   Interfaz: $($ip.InterfaceAlias) - IP: $($ip.IPAddress)"
-}
-Write-Host ""
+} 
+Write-Host "" 
 
-Write-Host "ESPACIO EN DISCO:" -ForegroundColor Yellow
-Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Used -ne $null } | Format-Table Name, @{Label = "Size(GB)"; Expression = { [math]::Round($_.Used / 1GB + $_.Free / 1GB, 2) } }, @{Label = "Used(GB)"; Expression = { [math]::Round($_.Used / 1GB, 2) } }, @{Label = "Free(GB)"; Expression = { [math]::Round($_.Free / 1GB, 2) } }, @{Label = "Use%"; Expression = { [math]::Round(($_.Used / ($_.Used + $_.Free)) * 100, 2) } } -AutoSize
+Write-Host "Espacio en Disco: "
+Get-PSDrive -PSProvider FileSystem | Where-Object {$_.Used -ne $null} | Format-Table Name, @{Label="Size(GB)";Expression={[math]::Round($_.Used/1GB + $_.Free/1GB, 2)}}, @{Label="Used(GB)";Expression={[math]::Round($_.Used/1GB, 2)}}, @{Label="Free(GB)";Expression={[math]::Round($_.Free/1GB, 2)}}, @{Label="Use%";Expression={[math]::Round(($_.Used/($_.Used + $_.Free)) * 100, 2)}} -AutoSize
 Write-Host ""
